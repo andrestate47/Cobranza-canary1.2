3d208edeb23b9b9820187a79b44af2d5ddd502df
Administrator <admin@localhost>
2026-06-01 19:06:18 -0500

    chore: remove backup files that conflict with nextjs compilation

--- app/api/auth/[...nextauth]/route - copia.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth"
-import { authOptions } from "@/lib/auth"
-
-const handler = NextAuth(authOptions)
-
-export const GET = handler
-export const POST = handler
-export const runtime = 'nodejs'
-export const dynamic = 'force-dynamic'
